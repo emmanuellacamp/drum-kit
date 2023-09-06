6521d18823db338d59e29e6ae64009690b58eca4
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2023-09-06 21:13:08 +0100

    linked javascript file

---
+++ index.js
@@ -0,0 +1,15 @@
+const buttonClicked = document.querySelectorAll(".drum")
+
+for (let i = 0; i < buttonClicked.length; i++){
+  buttonClicked[i].addEventListener("click", function (){
+    alert("I got clicked")
+  })
+}
+
+
+
+
+
+
+
+ 
