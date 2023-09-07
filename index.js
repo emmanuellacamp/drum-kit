a1cc17814bef2e9624b0c07b4941366fb5e22df7
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2023-09-07 10:27:29 +0100

    added audio to the buttons

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const buttonClicked = document.querySelectorAll(".drum")
 
 for (let i = 0; i < buttonClicked.length; i++){
   buttonClicked[i].addEventListener("click", function (){
-    alert("I got clicked")
+   let audio = new Audio("./sounds/tom-1.mp3")
+   audio.play();
   })
+  
 }
 
 
-
+//var audio = new Audio ('audio_file.mp3);
+//audio.play();
 
 
 
